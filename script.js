e1fb601538c797494101640a87613ce52d6f90d6
aniloli42 <aniloli42@gmail.com>
2021-06-19 21:01:30 +0545

    changes in file

--- script.js
+++ script.js
@@ -1,237 +1,287 @@
-let servicePrice = 0;
-let addTax = 0;
-let priceMessage = "";
-let inputValues = [];
-let displayPrice = document.getElementById("displayResult");
-let mapMarked = false;
-const removeMark = document.getElementById("removeMark");
+// Variables For map
 let map;
-let getDistance;
-let getTime;
-let secondMarkCords;
-var directionService;
-var directionsDisplay;
-let placeEntered = false;
-const distanceBox = document.getElementById("distance");
-const timeBox = document.getElementById("time");
-var placeAddress = document.getElementById("placeAddress");
-let placeMessage = document.getElementById("placeMessage");
-var options = {
+let marker;
+let directionsService;
+let directionsDisplay;
+let getClickPosition;
+const initialMapParameters = {
   zoom: 15,
+  disableDefaultUI: true,
   center: { lat: 40.7474824, lng: 14.6324808 },
 };
-
-let defaultMarkers = {
+const mapDisplaySelector = document.getElementById("map");
+const markerDefaultParameters = {
   cords: { lat: 40.74741139701506, lng: 14.63262364268303 },
 };
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), options);
+// Variable for the Display and Calculations
+const placeInputBox = document.getElementById("placeAddress");
+const placeInputErrorMessage = document.getElementById("placeMessage");
+const placeEnteredSearchBtn = document.getElementById("placeBtn");
+let getDistance;
+let getTime;
+const distanceInputBox = document.getElementById("distance");
+const timeInputBox = document.getElementById("time");
+const calculateMapBtn = document.getElementById("calcMapBtn");
+const resetMapBtn = document.getElementById("resetMapBtn");
+const displayResultDiv = document.getElementById("displayResult");
+const yesTollRadio = document.getElementById("yesTollRadio");
+const noTollRadio = document.getElementById("noTollRadio");
+const calculatorMainForm = document.getElementById("calcMain");
+let tollRadio;
 
-  let marker = new google.maps.Marker();
-
-  //   Add the marker when clicked in particular part
-  google.maps.event.addListener(map, "click", function (event) {
-    if (mapMarked == false) {
-      mapMarked = true;
-      addMarker({
-        cords: event.latLng,
-      });
+// initial map function
+function intialSetMap() {
+  return new Promise((resolve, reject) => {
+    map = new google.maps.Map(mapDisplaySelector, initialMapParameters);
+    if (map != null || map != undefined) {
+      resolve();
+    } else {
+      reject();
     }
   });
-
-  //AutoComplete the search within the country Italy
-
-  var addOption = {
-    types: ["establishment"],
-    fields: ["formatted_address", "geometry", "name"],
-    price_level: 0,
-  };
-
-  var autocomplete1 = new google.maps.places.Autocomplete(
-    placeAddress,
-    addOption
-  );
-  autocomplete1.bindTo("bounds", map);
-
-  // Add the restriction search to Italy
-  autocomplete1.setComponentRestrictions({ country: ["IT"] });
-
-  // Locate the place
-  var placeBtn = document.getElementById("placeBtn");
-  placeBtn.addEventListener("click", () => {
-    if ((placeAddress.value != "") & (placeAddress.value != undefined)) {
-      placeEntered = true;
-      placeEnter = placeAddress.value;
-      calcRoute(placeEnter);
-    }
-  });
-
-  addMarker(defaultMarkers);
-
-  // marker function
-  function addMarker(props) {
-    marker.setPosition(props.cords);
-    marker.setMap(map);
-    if (mapMarked) {
-      secondMarkCords = {
-        cords: {
-          lat: props.cords.lat(),
-          lng: props.cords.lng(),
-        },
-      };
-      calcRoute(secondMarkCords.cords);
-    }
-    removeMarked();
-  }
-
-  // create a Directions service object to use the route method
-  directionService = new google.maps.DirectionsService();
-
-  // create a DirectionsRenderrer object which will using to display the route
-  directionsDisplay = new google.maps.DirectionsRenderer();
-
-  // bind the directionsRenderrer to the map
-  directionsDisplay.setMap(map);
-
-  // function of calculate the route
-  function calcRoute(secondValue) {
-    // create request
-    var request = {
-      origin: defaultMarkers.cords,
-      destination: secondValue,
-      travelMode: "DRIVING", // DRIVING can change to BICYCLE, WALKING
-      unitSystem: google.maps.UnitSystem.METRIC,
-    };
-
-    // pass the request to the route method
-    directionService.route(request, (result, status) => {
-      if (status == google.maps.DirectionsStatus.OK) {
-        // get distance and time
-        getDistanceInMeter = result.routes[0].legs[0].distance.value;
-        getTimeInseconds = result.routes[0].legs[0].duration.value;
-
-        getDistance = getDistanceInMeter / 1000;
-        getTime = getTimeInseconds / 60;
-        getDistance = Math.round(getDistance.toFixed(2));
-        getTime = Math.round(getTime.toFixed(2));
-
-        distanceBox.value = getDistance;
-        timeBox.value = getTime;
-
-        directionsDisplay.setDirections(result);
-      } else {
-        map.setCenter(options.center);
-        placeMessage.innerHTML =
-          "Percorso non valido o Percorso non disponibile";
-        placeMessage.style.display = "block";
-        setTimeout(() => {
-          placeMessage.innerHTML = "";
-          placeMessage.style.display = "none";
-          placeAddress.value = "";
-        }, 3000);
-      }
-    });
-  }
-
-  //   remove mark
-  function removeMarked() {
-    removeMark.addEventListener("click", () => {
-      directionsDisplay.setDirections({ routes: [] });
-      marker.setMap(null);
-      secondMarkCords = "";
-      calcMainForm.reset();
-      mapMarked = false;
-      map.setCenter(options.center);
-      map.setZoom(15);
-      addMarker(defaultMarkers);
-      displayResult.style.display = "none";
-      placeAddress.value = "";
-    });
-  }
 }
 
-class getValues {
-  constructor(distance, time, toll) {
-    this.distance = distance;
-    this.time = time;
-    this.toll = toll;
-  }
+intialSetMap()
+  .then(markerFunction())
+  .catch((error) => {
+    console.log("that");
+  });
 
-  calculatePrice() {
-    if ((this.time <= 25) & (this.distance <= 20)) {
-      servicePrice = 0;
-      addTax = 0;
-    } else if ((this.time <= 35) & (this.distance <= 20)) {
-      if (this.toll == "no") {
-        servicePrice = 5;
-        addTax = 0;
-      } else {
-        servicePrice = 5;
-        addTax = 3;
-      }
-    } else if ((this.time <= 30) & (this.distance <= 30)) {
-      servicePrice = 10;
-      addTax = 0;
-    } else if ((this.time <= 37) & (this.distance <= 40)) {
-      if (this.toll == "no") {
-        servicePrice = 15;
-        addTax = 0;
-      } else {
-        servicePrice = 15;
-        addTax = 5;
-      }
-    } else if ((this.time <= 44) & (this.distance <= 43)) {
-      servicePrice = 20;
-      addTax = 0;
-    } else {
-      priceMessage = "Servizio fuori copertura";
+function markerFunction() {
+  marker = new google.maps.Marker();
+}
+
+// set the default marker into the serivce center
+marker.setPosition(markerDefaultParameters.cords);
+marker.setMap(map);
+
+// Listen the click in the google map
+google.maps.event.addListener(map, "click", (event) => {
+  placeInputBox.value = "";
+  getClickPosition = event.latLng;
+  displayRoute(getClickPosition);
+});
+
+// Autocomplete Parameters
+const autoCompleteParams = {
+  types: ["establishment"],
+  fields: ["formatted_address", "geometry", "name"],
+  price_level: 0,
+};
+
+// Initialize the autocomplete
+const autoCompleteSearch = new google.maps.places.Autocomplete(
+  placeInputBox,
+  autoCompleteParams
+);
+
+// Bind the autocomplete into the map
+autoCompleteSearch.bindTo("bounds", map);
+
+// Add the restriction search to Italy
+autoCompleteSearch.setComponentRestrictions({ country: ["IT"] });
+
+// route using place address
+placeEnteredSearchBtn.addEventListener("click", () => {
+  if ((placeInputBox.value != "") & (placeInputBox.value != undefined)) {
+    directionsDisplay.setDirections({ routes: [] });
+    displayRoute(placeInputBox.value);
+  } else {
+    placeInputErrorMessage.innerHTML = "Inserisci l'indirizzo";
+    placeMessage.style.display = "block";
+    setTimeout(() => {
+      placeInputBox.value = "";
+      placeInputErrorMessage.innerHTML = "";
+      placeMessage.style.display = "none";
+    }, 3000);
+  }
+});
+
+// Initialize the Direction Service Object
+directionsService = new google.maps.DirectionsService();
+
+//Initialize the DirectionsRenderrer object
+directionsDisplay = new google.maps.DirectionsRenderer();
+
+// bind the directions Renderer
+directionsDisplay.setMap(map);
+
+// function routing the distance to the client location
+function displayRoute(clientLocationPoint) {
+  // generate the route Parameters
+  let routeParams = {
+    origin: markerDefaultParameters.cords,
+    destination: clientLocationPoint,
+    travelMode: "DRIVING",
+    unitSystem: google.maps.UnitSystem.METRIC,
+  };
+
+  //   route the map using directionsService
+  directionsService.route(routeParams, (result, status) => {
+    //   if the result is found
+    if (status == google.maps.DirectionsStatus.OK) {
+      // remove the default marker for the show the origin single marked
+      marker.setMap(null);
+
+      //get the value form the map of route
+      getDistance = result.routes[0].legs[0].distance.value;
+      getTime = result.routes[0].legs[0].duration.value;
+      getDistance = getDistance / 1000;
+      getTime = getTime / 60;
+      getDistance = Math.round(getDistance);
+      getTime = Math.round(getTime);
+
+      //  set the value into the input box
+      distanceInputBox.value = getDistance + " km";
+      timeInputBox.value = getTime + " minuti";
+
+      // display the route
+      directionsDisplay.setDirections(result);
+    }
+    // if the result is invalid or not geocoded
+    if (status == google.maps.DirectionsStatus.NOT_FOUND) {
+      marker.setPosition(markerDefaultParameters.cords);
+      marker.setMap(map);
+      map.setCenter(initialMapParameters.center);
+      placeInputErrorMessage.innerHTML = "posto non trovato";
+      placeMessage.style.display = "block";
+      setTimeout(() => {
+        placeInputBox.value = "";
+        placeInputErrorMessage.innerHTML = "";
+        placeMessage.style.display = "none";
+      }, 3000);
     }
 
-    if (priceMessage == "") {
-      displayResult.innerHTML = `Prezzo Totale: &euro; ${
-        servicePrice + addTax
-      }`;
-      displayResult.style.backgroundColor = "#4bb543";
-      displayResult.style.display = "block";
-    } else {
+    // if the result is not available
+    if (status == google.maps.DirectionsStatus.ZERO_RESULTS) {
+      marker.setPosition(markerDefaultParameters.cords);
+      marker.setMap(map);
+      map.setCenter(initialMapParameters.center);
+      placeInputErrorMessage.innerHTML =
+        "Percorso non valido o Percorso non disponibile";
+      placeMessage.style.display = "block";
+      setTimeout(() => {
+        placeInputBox.value = "";
+        placeInputErrorMessage.innerHTML = "";
+        placeMessage.style.display = "none";
+      }, 3000);
+    }
+  });
+}
+
+// when the calculate button
+let countDownTimer;
+let priceMessage = "";
+let servicePrice;
+let addTax;
+calculateMapBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    (getDistance != undefined) & (getDistance != "") ||
+    (getTime != undefined) & (getTime != "")
+  ) {
+    if (getTime > 44 || getDistance > 43) {
+      priceMessage = `Servizio fuori copertura`;
       displayResult.style.backgroundColor = "red";
       displayResult.innerHTML = `${priceMessage}`;
       displayResult.style.display = "block";
       setTimeout(() => {
         displayResult.style.display = "none";
+        calculatorMainForm.reset();
+        resetMapBtn.value = `Reset`;
+        directionsDisplay.setDirections({ routes: [] });
+        marker.setPosition(markerDefaultParameters.cords);
+        marker.setMap(map);
+        map.setZoom(15);
+        map.setCenter(initialMapParameters.center);
       }, 3000);
+    } else {
+      servicePrice = 0;
+      addTax = 0;
+      noTollRadio.checked ? (tollRadio = "no") : (tollRadio = "yes");
+      priceMessage = "";
+      calculateTotal();
+      countDownTimer = setInterval(runCountDownTimer, 1000);
     }
-  }
-}
-
-const calcMainForm = document.getElementById("calcMain");
-calcMainForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  inputValues = [];
-  servicePrice = 0;
-  addTax = 0;
-  priceMessage = "";
-
-  const formData = new FormData(e.target);
-
-  for (const form of formData) {
-    inputValues.push(form[1]);
-  }
-  if ((inputValues[0] != "") & (inputValues[1] != "")) {
-    const calcObj = new getValues(
-      inputValues[0],
-      inputValues[1],
-      inputValues[2]
-    );
-    calcObj.calculatePrice();
   } else {
-    displayResult.innerHTML = `Seleziona nella mappa`;
-    displayResult.style.display = "block";
     displayResult.style.backgroundColor = "red";
+    displayResult.innerHTML = `seleziona nella mappa o inserisci l'indirizzo`;
+    displayResult.style.display = "block";
     setTimeout(() => {
-      displayResult.innerHTML = "";
       displayResult.style.display = "none";
     }, 3000);
   }
+});
+
+// calculate the total price
+function calculateTotal() {
+  if ((getTime <= 25) & (getDistance <= 20)) {
+    servicePrice = 0;
+    addTax = 0;
+  } else if ((getTime <= 35) & (getDistance <= 20)) {
+    if (tollRadio == "no") {
+      servicePrice = 5;
+      addTax = 0;
+    } else {
+      servicePrice = 5;
+      addTax = 3;
+    }
+  } else if ((getTime <= 30) & (getDistance <= 30)) {
+    servicePrice = 10;
+    addTax = 0;
+  } else if ((getTime <= 37) & (getDistance <= 40)) {
+    if (tollRadio == "no") {
+      servicePrice = 15;
+      addTax = 0;
+    } else {
+      servicePrice = 15;
+      addTax = 5;
+    }
+  } else if ((getTime <= 44) & (getDistance <= 43)) {
+    servicePrice = 20;
+    addTax = 0;
+  }
+  console.log(servicePrice, addTax);
+  if (priceMessage == "") {
+    displayResult.innerHTML = `Prezzo Totale: &euro; ${servicePrice + addTax}`;
+    displayResult.style.backgroundColor = "#4bb543";
+    displayResult.style.display = "block";
+  }
+}
+
+// countdown display
+let count = 15;
+
+function runCountDownTimer() {
+  if (count > 1) {
+    count = count - 1;
+    resetMapBtn.value = `Reset (${count})`;
+  } else {
+    clearInterval(countDownTimer);
+    count = 15;
+    calculatorMainForm.reset();
+    resetMapBtn.value = `Reset`;
+    displayResult.style.display = "none";
+    directionsDisplay.setDirections({ routes: [] });
+    marker.setPosition(markerDefaultParameters.cords);
+    marker.setMap(map);
+    map.setZoom(15);
+    map.setCenter(initialMapParameters.center);
+  }
+}
+
+// when the reset Button Clicked
+resetMapBtn.addEventListener("click", () => {
+  clearInterval(countDownTimer);
+  calculatorMainForm.reset();
+  count = 15;
+  displayResult.style.display = "none";
+  resetMapBtn.value = `Reset`;
+  placeInputBox.value = "";
+  directionsDisplay.setDirections({ routes: [] });
+  marker.setPosition(markerDefaultParameters.cords);
+  marker.setMap(map);
+  map.setZoom(15);
+  map.setCenter(initialMapParameters.center);
 });
