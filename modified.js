1fc0b090fd34fe7120e732c116d9893d663b210a
aniloli42 <aniloli42@gmail.com>
2021-06-19 18:55:16 +0545

    auto reset implemented

--- modified.js
+++ modified.js
@@ -6,6 +6,7 @@ let directionsDisplay;
 let getClickPosition;
 const initialMapParameters = {
   zoom: 15,
+  disableDefaultUI: true,
   center: { lat: 40.7474824, lng: 14.6324808 },
 };
 const mapDisplaySelector = document.getElementById("map");
@@ -15,9 +16,17 @@ const markerDefaultParameters = {
 
 // Variable for the Display and Calculations
 const placeInputBox = document.getElementById("placeAddress");
+const placeInputErrorMessage = document.getElementById("placeMessage");
 const placeEnteredSearchBtn = document.getElementById("placeBtn");
-let getDistanceInMeter;
-let geTime;
+let getDistance;
+let getTime;
+const distanceInputBox = document.getElementById("distance");
+const timeInputBox = document.getElementById("time");
+const calculateMapBtn = document.getElementById("calcMapBtn");
+const resetMapBtn = document.getElementById("resetMapBtn");
+const displayResultDiv = document.getElementById("displayResult");
+const yesTollRadio = document.getElementById("yesTollRadio");
+const noTollRadio = document.getElementById("noTollRadio");
 
 function intialSetMap() {
   return new Promise((resolve, reject) => {
@@ -46,6 +55,7 @@ marker.setMap(map);
 
 // Listen the click in the google map
 google.maps.event.addListener(map, "click", (event) => {
+  placeInputBox.value = "";
   getClickPosition = event.latLng;
   displayRoute(getClickPosition);
 });
@@ -69,9 +79,10 @@ autoCompleteSearch.bindTo("bounds", map);
 // Add the restriction search to Italy
 autoCompleteSearch.setComponentRestrictions({ country: ["IT"] });
 
-// route the
+// route using place address
 placeEnteredSearchBtn.addEventListener("click", () => {
   if ((placeInputBox.value != null) & (placeInputBox.value != undefined)) {
+    directionsDisplay.setDirections({ routes: [] });
     displayRoute(placeInputBox.value);
   }
 });
@@ -97,11 +108,89 @@ function displayRoute(clientLocationPoint) {
 
   //   route the map using directionsService
   directionsService.route(routeParams, (result, status) => {
+    //   if the result is found
     if (status == google.maps.DirectionsStatus.OK) {
       // remove the default marker for the show the origin single marked
       marker.setMap(null);
+
+      //get the value form the map of route
+      getDistance = result.routes[0].legs[0].distance.value;
+      getTime = result.routes[0].legs[0].duration.value;
+      getDistance = getDistance / 1000;
+      getTime = getTime / 60;
+      getDistance = Math.round(getDistance);
+      getTime = Math.round(getTime);
+
+      //  set the value into the input box
+      distanceInputBox.value = getDistance + " km";
+      timeInputBox.value = getTime + " minuti";
+
       // display the route
       directionsDisplay.setDirections(result);
     }
+    // if the result is invalid or not geocoded
+    if (status == google.maps.DirectionsStatus.NOT_FOUND) {
+      marker.setPosition(markerDefaultParameters.cords);
+      marker.setMap(map);
+      map.setCenter(initialMapParameters.center);
+      placeInputErrorMessage.innerHTML = "posto non trovato";
+      placeMessage.style.display = "block";
+      setTimeout(() => {
+        placeInputBox.value = "";
+        placeInputErrorMessage.innerHTML = "";
+        placeMessage.style.display = "none";
+      }, 3000);
+    }
+
+    // if the result is not available
+    if (status == google.maps.DirectionsStatus.ZERO_RESULTS) {
+      marker.setPosition(markerDefaultParameters.cords);
+      marker.setMap(map);
+      map.setCenter(initialMapParameters.center);
+      placeInputErrorMessage.innerHTML =
+        "Percorso non valido o Percorso non disponibile";
+      placeMessage.style.display = "block";
+      setTimeout(() => {
+        placeInputBox.value = "";
+        placeInputErrorMessage.innerHTML = "";
+        placeMessage.style.display = "none";
+      }, 3000);
+    }
   });
 }
+
+// when the calculate button
+let countDownTimer;
+calculateMapBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  countDownTimer = setInterval(runCountDownTimer, 1000);
+});
+
+// countdown display
+let count = 15;
+function runCountDownTimer() {
+  if (count > 1) {
+    count = count - 1;
+    resetMapBtn.value = `Reset (${count})`;
+  } else {
+    clearInterval(countDownTimer);
+    count = 15;
+    resetMapBtn.value = `Reset`;
+    directionsDisplay.setDirections({ routes: [] });
+    marker.setPosition(markerDefaultParameters.cords);
+    marker.setMap(map);
+    map.setCenter(initialMapParameters.center);
+  }
+}
+
+// when the reset Button Clicked
+resetMapBtn.addEventListener("click", () => {
+  clearInterval(countDownTimer);
+  count = 15;
+  resetMapBtn.value = `Reset`;
+  placeInputBox.value = "";
+  directionsDisplay.setDirections({ routes: [] });
+  marker.setPosition(markerDefaultParameters.cords);
+  marker.setMap(map);
+  map.setCenter(initialMapParameters.center);
+});
